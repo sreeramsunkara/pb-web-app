249707f3ceb2bc3d8fef11379bf55906ba561131
Sreeram Sunkara <sunkarasr@sunkarasr.local>
2020-05-13 17:40:18 -0400

    first commit

---
+++ src/main/java/io/pivotal/web/WebApplication.java
@@ -0,0 +1,16 @@
+package io.pivotal.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
